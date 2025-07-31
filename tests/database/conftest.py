35931e54e236ffd8dfb94aa3e8c936a4589507d7
Skhendle@gmail.com <skhendle@gmail.com>
2025-07-31 22:22:19 +0200

    database session manager refactor

--- tests/database/conftest.py
+++ tests/database/conftest.py
@@ -2,7 +2,7 @@ import pytest
 import json
 from unittest.mock import patch
 from app.utils.config.loader import ConfigLoader
-from app.database import DatabaseSessionManager
+from app.database.session_manager import DatabaseSessionManager
 
 
 @pytest.fixture(scope="session")
@@ -34,22 +34,23 @@ def test_role_data():
 
 @pytest.fixture(scope="function")
 def test_session():
-    # Mock the get_config method of ConfigLoader to return an in-memory SQLite DB config
-    with patch.object(
-        ConfigLoader,
-        "get_config",
-        return_value={
-            "database_url": "sqlite:///:memory:",
-            "environment": "test",
-            "cor_origins": {"allowed": ["*"], "blocked": []},
-            "jwt": {},
-            "email": {},
-            "version": "0.1.0",
-            "name": "Userverse",
-            "description": "Mocked config for test",
-        },
-    ):
-        db_manager = DatabaseSessionManager()
-        session = db_manager.session_object()
-        yield session
-        session.close()
+    test_configs = {
+        "database_url": "sqlite:///:memory:",
+        "environment": "test",
+        "cor_origins": {"allowed": ["*"], "blocked": []},
+        "jwt": {},
+        "email": {},
+        "version": "0.1.0",
+        "name": "Userverse",
+        "description": "Mocked config for test",
+    }
+
+    db_manager = DatabaseSessionManager(configs=test_configs)
+    engine = db_manager.engine
+
+    # Clean slate schema
+    db_manager._base.metadata.create_all(bind=engine)
+
+    session = db_manager.session_object()
+    yield session
+    session.close()
