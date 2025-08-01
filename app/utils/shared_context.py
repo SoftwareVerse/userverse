624fdd89232a1218c9ec5e9dd37774eb0a1ab658
Skhendle@gmail.com <skhendle@gmail.com>
2025-08-01 18:36:31 +0200

    shared context for repo & logic

---
+++ app/utils/shared_context.py
@@ -0,0 +1,71 @@
+# app/services/base.py
+from typing import Any, Dict
+from decimal import Decimal
+from datetime import datetime, date
+from app.models.user.user import UserRead
+from app.utils.logging import logger
+
+
+class SharedContext:
+    def __init__(
+        self,
+        configs: dict,
+        user: UserRead,
+        db_session,
+    ):
+        self.configs: dict = configs
+        self.user: UserRead = user
+        self.db_session = db_session
+
+    def get_user_email(self) -> str:
+        """
+            -  Returns the email of the user associated with this context.
+        """
+        return self.user.email
+
+    def get_user(self) -> UserRead:
+        """
+            -  Returns the user object associated with this context.
+        """
+        return self.user
+
+    def log_context(self) -> Dict[str, Any]:
+        """
+            -  Returns a dictionary containing the user email for logging purposes.
+        """
+        return {
+            "user_email": self.get_user_email(),
+        }
+
+    def log_info(self, message: str):
+        """
+            -  Logs an informational message with the user email.
+        """
+        logger.info("user_email=%s, message=%s", self.get_user_email(), message)
+
+    def log_error(self, message: str):
+        """
+            -  Logs an error message with the user email.
+        """
+        logger.error(
+            "user_email=%s, message=%s",
+            self.get_user_email(),
+            message,
+        )
+
+    @staticmethod
+    def safe_json(data: Any) -> Any:
+        """
+        - Converts various data types to a JSON-serializable format.
+        - Handles datetime, date, Decimal, dict, and list types.
+        - Returns the original data for other types.
+        """
+        if isinstance(data, (datetime, date)):
+            return data.isoformat()
+        if isinstance(data, Decimal):
+            return float(data)  # or str(data) if you prefer string representation
+        if isinstance(data, dict):
+            return {k: SharedContext.safe_json(v) for k, v in data.items()}
+        if isinstance(data, list):
+            return [SharedContext.safe_json(i) for i in data]
+        return data
